4c56a93a81c98cae37e3303d67ebfeda51c32c0c
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-24 19:06:43 +0530

    Add project files.

---
+++ Food_Search/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿namespace Food_Search.Models
+{
+    public class Login
+    {
+       
+            public int login_id { get; set; }
+            public int reg_id { get; set; }
+            public string? email { get; set; }
+            public string? password { get; set; }
+            public string? login_type { get; set; }
+        
+    }
+}
