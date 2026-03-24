7e0b4a18b4f7b7c53c8f9036625dd95b02c8b7c3
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-24 19:10:35 +0530

    Updated

---
+++ Food_Search/Models/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace Food_Search.Models
+{
+    public class Owner
+    {
+        public int owner_id { get; set; }
+        public string? owner_name { get; set; }
+        public string? owner_phone { get; set; }
+        public string? owner_address { get; set; }
+    }
+}
