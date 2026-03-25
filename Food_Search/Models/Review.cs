a868c57569a1cfa8527c282ce01c84dc2c022ea0
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-25 18:46:30 +0530

    Updated

---
+++ Food_Search/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Food_Search.Models
+{
+    public class Review
+    {
+        public int review_id { get; set; }
+        public int hotel_id { get; set; }
+        public int user_id { get; set; }
+        public int rating { get; set; }
+        public string? comment { get; set; }
+    }
+}
