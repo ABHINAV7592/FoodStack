dc31a35dd6e95161c80991ac918dcc28b2e529f0
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-25 19:46:59 +0530

    Updated

---
+++ Food_Search/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Food_Search.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Search.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly CategoryDB _catDb;
+
+        public AdminController(CategoryDB catDb)
+        {
+            _catDb = catDb;
+        }
+
+        public IActionResult Categories()
+        {
+            var list = _catDb.GetCategories();
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(string category_name)
+        {
+            _catDb.AddCategory(category_name);
+            return RedirectToAction("Categories");
+        }
+    }
+}
