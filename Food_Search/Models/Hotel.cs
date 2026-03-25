fc3517d105338b4ba7049a10ec30e6aba0622ad2
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-25 15:48:56 +0530

    Updated

---
+++ Food_Search/Models/Hotel.cs
@@ -0,0 +1,13 @@
+﻿namespace Food_Search.Models
+{
+    public class Hotel
+    {
+        public int hotel_id { get; set; }
+        public string? hotel_name { get; set; }
+        public string? hotel_description { get; set; }
+        public int place_id { get; set; }
+        public int category_id { get; set; }
+        public int owner_id { get; set; }
+        public string? hotel_image { get; set; }
+    }
+}
