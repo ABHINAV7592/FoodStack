8cbe7fa3efe87b3d94156131ec845c91f7232ee2
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-26 19:31:54 +0530

    Updated

---
+++ Food_Search/Controllers/HotelController.cs
@@ -0,0 +1,49 @@
+﻿using Food_Search.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Search.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly HotelDB _db;
+        private readonly IWebHostEnvironment _env;
+
+        public HotelController(HotelDB db, IWebHostEnvironment env)
+        {
+            _db = db;
+            _env = env;
+        }
+
+        public IActionResult AddHotel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddHotel(Hotel model, IFormFile file)
+        {
+            string fileName = null;
+
+            if (file != null)
+            {
+                fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                string path = Path.Combine(_env.WebRootPath, "images", fileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await file.CopyToAsync(stream);
+            }
+
+            model.hotel_image = "/images/" + fileName;
+            model.owner_id = (int)HttpContext.Session.GetInt32("reg_id");
+
+            _db.AddHotel(model);
+
+            return RedirectToAction("AddHotel");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
