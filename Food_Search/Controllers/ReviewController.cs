40b2680939c27a0e033868179de991bccba1cdf5
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-31 19:23:15 +0530

    Updated

---
+++ Food_Search/Controllers/ReviewController.cs
@@ -0,0 +1,31 @@
+﻿using Food_Search.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Search.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        private readonly ReviewDB _db;
+
+        public ReviewController(ReviewDB db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public IActionResult AddReview(int hotel_id, int rating, string comment)
+        {
+            int userId = (int)HttpContext.Session.GetInt32("reg_id");
+
+            _db.AddReview(hotel_id, userId, rating, comment);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
