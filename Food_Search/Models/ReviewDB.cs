1a6a1da4eb681098f50d208c58800acd28032ff5
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-03-31 15:36:26 +0530

    Updated

---
+++ Food_Search/Models/ReviewDB.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Food_Search.Models
+{
+    public class ReviewDB
+    {
+        private readonly string _con;
+
+        public ReviewDB(IConfiguration config)
+        {
+            _con = config.GetConnectionString("DefaultConnection");
+        }
+
+        public void AddReview(int hotelId, int userId, int rating, string comment)
+        {
+            using SqlConnection con = new(_con);
+            SqlCommand cmd = new("sp_add_review", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@hotel_id", hotelId);
+            cmd.Parameters.AddWithValue("@user_id", userId);
+            cmd.Parameters.AddWithValue("@rating", rating);
+            cmd.Parameters.AddWithValue("@comment", comment);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
